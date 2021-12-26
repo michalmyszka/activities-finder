12c79f5947581963436cba29c37bf135d02c368f
michal_myszka_cognitran <michal.myszka@cognitran.com>
2021-12-26 09:32:30 +0100

    Initial commit with AWS Amplify.

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,17 +11,30 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/vue'
+import ExploreContainer from '@/components/ExploreContainer.vue'
 
-export default  {
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+export default {
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
 }
 </script>
